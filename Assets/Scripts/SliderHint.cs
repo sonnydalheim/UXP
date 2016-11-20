e5cf013103bb91f3a89fbdd6fad48e54a6362eb4
sonnydalheim <sonny_dalheim@hotmail.com>
2016-11-20 19:51:45 +0100

    Temporary slider hint added.

---
+++ Assets/Scripts/SliderHint.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SliderHint : MonoBehaviour {
+
+
+	public PlungerScript plungerScript;
+	public Image SliderHintImage;
+	Color sliderHintColor;
+
+	bool transparent;
+
+	// Use this for initialization
+	void Start () {
+		sliderHintColor = SliderHintImage.color;
+		plungerScript = GameObject.FindWithTag("Platform").GetComponent<PlungerScript>();
+	}
+
+	IEnumerator Fade() {
+		if(plungerScript.platformLaunched == false) {
+			while(sliderHintColor.a > 0 && transparent == false) {
+				sliderHintColor.a -= Time.deltaTime/25;
+				SliderHintImage.color = sliderHintColor;
+				yield return null;
+			}
+			transparent = true;
+
+			while(sliderHintColor.a < 1 && transparent == true) {
+				sliderHintColor.a += Time.deltaTime/25;
+				SliderHintImage.color = sliderHintColor;
+				yield return null;
+			}
+			transparent = false;
+		}
+		else {
+			Destroy(SliderHintImage);
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		StartCoroutine("Fade");
+	}
+}
